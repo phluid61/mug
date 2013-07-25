59f2eca90b43495f9ec895a3a1a19f32d352f5ef
Matthew Kerwin <matthew@kerwin.net.au>
2013-07-25 10:28:36 +1000

    alphabetised
    [ci skip]

--- lib/mug.rb
+++ lib/mug.rb
@@ -1,7 +1,7 @@
 
 require_relative 'mug/bool'
-require_relative 'mug/hashmap'
 require_relative 'mug/fragile-method-chain'
+require_relative 'mug/hashmap'
 require_relative 'mug/iterator/for'
 require_relative 'mug/iterator/method'
 require_relative 'mug/maybe'
