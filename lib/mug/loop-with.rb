12fe6711aac9cf77b8230e1183a811a242f8905f
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-11-01 10:30:41 +1000

    fix loop-with error logic, add more tests

--- lib/mug/loop-with.rb
+++ lib/mug/loop-with.rb
@@ -3,19 +3,19 @@ module Kernel
   #
   # Repeatedly executes the block, yielding the current iteration
   # count, which starts from +offset+. If no block is given, returns
-  # a new Enumerator that includes the iteration count, starting
-  # from +offset+
+  # an Enumerator.
   #
   def loop_with_index(offset=0)
     return c=enum_for(:loop_with_index, offset) unless block_given?
     c = 0 + offset
-    while true
-      yield c
-      c += 1
+    begin
+      while true
+        yield c
+        c += 1
+      end
+    rescue StopIteration
     end
-  rescue StopIteration
-  ensure
-    return c
+    c
   end
 
   #
@@ -23,12 +23,13 @@ module Kernel
   #
   def loop_with_object(obj)
     return obj=enum_for(:loop_with_object, obj) unless block_given?
-    while true
-      yield obj
+    begin
+      while true
+        yield obj
+      end
+    rescue StopIteration
     end
-  rescue StopIteration
-  ensure
-    return obj
+    obj
   end
 end
 
--- test/test-loop-with.rb
+++ test/test-loop-with.rb
@@ -13,11 +13,10 @@ class Test_loop_with < Test::Unit::TestCase
     end
     y = loop_with_index do |i|
       b << i
-      throw StopIteration if b.length >= 3
+      raise StopIteration if b.length >= 3
     end
     assert_equal( [0,1,2], a )
     assert_equal( [0,1,2], b )
-    assert_equal( 2, x )
     assert_equal( 2, y )
   end
   def test_loop_with_index__block_offset
@@ -29,46 +28,98 @@ class Test_loop_with < Test::Unit::TestCase
     end
     y = loop_with_index(10) do |i|
       b << i
-      throw StopIteration if b.length >= 3
+      raise StopIteration if b.length >= 3
     end
     assert_equal( [10,11,12], a )
     assert_equal( [10,11,12], b )
-    assert_equal( 12, x )
     assert_equal( 12, y )
   end
   def test_loop_with_index__enum
+    enum = loop_with_index
+    assert_respond_to( enum, :each )
+
     a = []
     b = []
-    enum = loop_with_index
     x = enum.each do |i|
       a << i
       break if a.length >= 3
     end
     y = enum.each do |i|
       b << i
-      throw StopIteration if b.length >= 3
+      raise StopIteration if b.length >= 3
     end
     assert_equal( [0,1,2], a )
     assert_equal( [0,1,2], b )
-    assert_equal( 2, x )
     assert_equal( 2, y )
   end
   def test_loop_with_index__enum_offset
+    enum = loop_with_index(10)
+    assert_respond_to( enum, :each )
+
     a = []
     b = []
-    enum = loop_with_index(10)
     x = enum.each do |i|
       a << i
       break if a.length >= 3
     end
     y = enum.each do |i|
       b << i
-      throw StopIteration if b.length >= 3
+      raise StopIteration if b.length >= 3
     end
     assert_equal( [10,11,12], a )
     assert_equal( [10,11,12], b )
-    assert_equal( 12, x )
     assert_equal( 12, y )
+  end
+
+  def test_loop_with_object__block
+    o = "x"
+    a = []
+    b = []
+    x = loop_with_object(o) do |e|
+      a << e
+      break if a.length >= 3
+    end
+    y = loop_with_object(o) do |e|
+      b << e
+      raise StopIteration if b.length >= 3
+    end
+    assert_equal( [o,o,o], a )
+    assert_equal( [o,o,o], b )
+    assert( y.equal?(o), "should return `obj`" )
+  end
+  def test_loop_with_object__enum
+    o = "x"
+    enum = loop_with_object(o)
+    assert_respond_to( enum, :each )
+
+    a = []
+    b = []
+    x = enum.each do |e|
+      a << e
+      break if a.length >= 3
+    end
+    y = enum.each do |e|
+      b << e
+      raise StopIteration if b.length >= 3
+    end
+    assert_equal( [o,o,o], a )
+    assert_equal( [o,o,o], b )
+    assert( y.equal?(o), "should return `obj`" )
+
+    c = []
+    loop_with_object(c).with_index do |a, i|
+      a << i
+      break if i >= 2
+    end
+    assert_equal( [0,1,2], c )
+  end
+
+  def test_loop_with_index__error
+    assert_raise(RuntimeError) { loop_with_index {|i| raise 'die' } }
+  end
+  def test_loop_with_object__error
+    a = []
+    assert_raise(RuntimeError) { loop_with_object(a) {|e| raise 'die' } }
   end
 
 end
