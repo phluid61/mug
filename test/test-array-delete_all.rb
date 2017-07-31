76ff6b5c416fd6c45c9eeff24a702c5619a59d6c
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2017-07-31 11:24:22 +1000

    use constants instead of globals in test

--- test/test-array-delete_all.rb
+++ test/test-array-delete_all.rb
@@ -1,39 +1,39 @@
 require 'test/unit'
 $VERBOSE = true
 
-$ARRAY = proc { [1,2,3,4,5,6] }
-$EVEN  = proc { |i| i % 2 == 0 }
-$E = [2,4,6]
-$O = [1,3,5]
+ARRAY = proc { [1,2,3,4,5,6] }
+EVEN  = proc { |i| i % 2 == 0 }
+E = [2,4,6]
+O = [1,3,5]
 
 require_relative '../lib/mug/array/delete_all'
 class Test_array_delete_all < Test::Unit::TestCase
   def test_delete_all__block
-    a = $ARRAY.call
-    result = a.delete_all &$EVEN
-    assert_equal( result, $E )
-    assert_equal( a, $O )
+    a = ARRAY.call
+    result = a.delete_all &EVEN
+    assert_equal( result, E )
+    assert_equal( a, O )
   end
   def test_delete_all__noblock
-    a = $ARRAY.call
+    a = ARRAY.call
     enum = a.delete_all
     assert( enum.is_a? Enumerator )
 
-    result = enum.each &$EVEN
-    assert_equal( result, $E )
-    assert_equal( a, $O )
+    result = enum.each &EVEN
+    assert_equal( result, E )
+    assert_equal( a, O )
   end
   def test_delete_all__all
-    a = $E.dup
-    result = a.delete_all &$EVEN
-    assert_equal( result, $E )
+    a = E.dup
+    result = a.delete_all &EVEN
+    assert_equal( result, E )
     assert( a.empty? )
   end
   def test_delete_all__none
-    a = $O.dup
-    result = a.delete_all &$EVEN
+    a = O.dup
+    result = a.delete_all &EVEN
     assert( result.empty? )
-    assert_equal( a, $O )
+    assert_equal( a, O )
   end
 end
 
