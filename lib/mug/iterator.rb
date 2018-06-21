0bf5ff13ecd714d7289d4e85397a78653918dddb
Matthew Kerwin <matthew@kerwin.net.au>
2018-06-21 21:56:19 +1000

    change Iterator to accept a block
    ... instead of the old-style 'method' parameter.
    
    Also make the Iterator class more accessible.

--- lib/mug/iterator.rb
+++ lib/mug/iterator.rb
@@ -1,2 +1,43 @@
-require_relative 'iterator/for'
-require_relative 'iterator/method'
+
+#
+# A special class of Enumerator that repeatedly invokes a method.
+#
+# Initially the method is send to the given +obj+, but subsequent
+# invocations are sent to the result of the previous invocation.
+#
+# Example:
+#
+#     0.iter_for(:next).take(5) #=> [1,2,3,4,5]
+#
+class Iterator < Enumerator
+  #
+  # Creates a new Iterator for method +meth+, to be
+  # called initially on object +obj+.
+  #
+  # All method calls will have +args+ as parameters.
+  #
+  def initialize obj, *args
+    super() do |y|
+      loop do
+        y << (obj = yield obj, *args)
+      end
+    end
+  end
+end
+
+=begin
+Copyright (c) 2013-2018, Matthew Kerwin <matthew@kerwin.net.au>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+=end
+
